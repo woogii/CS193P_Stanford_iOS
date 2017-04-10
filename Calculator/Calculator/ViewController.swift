7b14b4b9032e94c6a573b313f2e1819c0ad16e73
woogii <jhsw0201@naver.com>
2017-04-10 23:29:47 +0900

    feat : Create a project

---
+++ Calculator/Calculator/ViewController.swift
@@ -0,0 +1,65 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by siwook on 2017. 4. 10..
+//  Copyright © 2017년 siwook. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+  
+  @IBOutlet weak var display: UILabel!
+  var userIsInTheMiddleOfTyping = false
+  var displayValue : Double {
+    get {
+      return Double(display.text!)!
+    }
+    set {
+      display.text = String(newValue)
+    }
+  }
+  
+  @IBAction func touchDigit(_ sender: UIButton) {
+    let digit = sender.currentTitle!
+    
+    if userIsInTheMiddleOfTyping {
+      let textCurrentlyInDisplay = display.text!
+      display.text = textCurrentlyInDisplay + digit
+    } else {
+      display.text = digit
+      userIsInTheMiddleOfTyping = true
+    }
+    
+  }
+  
+  
+  
+  @IBAction func performOperation(_ sender: UIButton) {
+    
+    userIsInTheMiddleOfTyping = false
+    
+    if let mathematicalSymbol = sender.currentTitle {
+      
+      switch mathematicalSymbol {
+      case "π" :
+        displayValue = Double.pi
+      case "√" :
+        displayValue = sqrt(displayValue)
+      default :
+        break
+      }
+      
+    }
+  }
+  
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+  }
+  
+  
+  
+}
+
